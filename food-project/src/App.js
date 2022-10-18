8878c4935e2c07d63c60ac0535509d6ae367bbe2
Rasheem Khan <rasheem.khan94@gmail.com>
2022-10-18 13:53:23 -0400

    new commit

--- food-project/src/App.js
+++ food-project/src/App.js
@@ -1,9 +1,24 @@
-
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const apiKey = '7d62535ab0e841b593f0118af07f524f'
+
+  let [food, setFood] = useState([])
+
+  const getFood = async () => {
+    //.then((res) => res.json())
+    //.then((data) => console.log(data))
+    const response = await fetch (`https://api.spoonacular.com/recipes/716429/information?apiKey=${apiKey}&includeNutrition=true`)
+    const data = await response.json()
+    setFood(data)
+  }
+  
+  
+  
   return (
     <div className="App">
+
     
     </div>
   );
